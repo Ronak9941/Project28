5a1e2a91b5be8beee8ad91cc856e864dafe00568
Ronak9941 <81176751+Ronak9941@users.noreply.github.com>
2021-04-08 13:14:43 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -15,7 +15,7 @@ var chain;
 
 
 function preload(){
-	boy=loadImage("images/boy.png");
+	boy=loadImage("boy.png");
   }
 
 function setup() {
@@ -125,7 +125,7 @@ function mouseReleased(){
 function keyPressed(){
 if(keyCode === 32){
   Matter.Body.setPosition(stoneObj.body,{x:235,y:420})
-  launcherObject.attach(stoneObj.body);
+  
  
 }
 
